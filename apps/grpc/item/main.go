8ad51735a6e0212e91e07b91aac93452f2f58555
Rayato159 <rayato159@gmail.com>
2023-07-01 16:06:25 +0700

    -fix :bug: fix .env file path loader

--- apps/grpc/item/main.go
+++ apps/grpc/item/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/Rayato159/stupid-inventory/pkg/db"
 	"github.com/Rayato159/stupid-inventory/src/config"
@@ -9,7 +10,12 @@ import (
 )
 
 func main() {
-	cfg := config.NewConfig("./.env.grpc.item")
+	cfg := config.NewConfig(func() string {
+		if len(os.Args) > 1 {
+			return os.Args[1]
+		}
+		return "../../../.env.grpc.item"
+	}())
 
 	dbClient := db.DBConn(cfg)
 	defer dbClient.Disconnect(context.Background())
--- apps/http/user/main.go
+++ apps/http/user/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/Rayato159/stupid-inventory/pkg/db"
 	"github.com/Rayato159/stupid-inventory/src/config"
@@ -9,7 +10,12 @@ import (
 )
 
 func main() {
-	cfg := config.NewConfig("./.env.http.user")
+	cfg := config.NewConfig(func() string {
+		if len(os.Args) > 1 {
+			return os.Args[1]
+		}
+		return "../../../.env.http.user"
+	}())
 
 	dbClient := db.DBConn(cfg)
 	defer dbClient.Disconnect(context.Background())
